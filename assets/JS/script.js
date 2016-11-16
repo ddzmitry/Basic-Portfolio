b0a2dd8941efbcb99ca70bbea0e10318eb3762e9
Dzmitry Dubarau <ddzmitry@yahoo.com>
2016-11-16 00:57:18 -0500

    CSS added

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -26,25 +26,11 @@ document.addEventListener('DOMContentLoaded', function(){
 			if (min < 10) min = '0' + min;
 
 
-	c.innerHTML = hours + sep + min + ampm;
+	c.innerHTML = hours + sep + min + " " + ampm;
 
 
 	}
 
 });
-   var frmvalidator  = new Validator("contactus");
-    frmvalidator.EnableOnPageErrorDisplay();
-    frmvalidator.EnableMsgsTogether();
-    frmvalidator.addValidation("name","req","Please provide your name");
- 
-    frmvalidator.addValidation("email","req",
-                "Please provide your email address");
- 
-    frmvalidator.addValidation("email","email",
-                "Please provide a valid email address");
- 
-    frmvalidator.addValidation("message","maxlen=2048",
-                "The message is too long!(more than 2KB!)");
-
-
+   
 })();
