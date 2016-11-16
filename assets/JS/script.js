7cee9d91218780db0662ced314fa362e78bf79bb
Dzmitry Dubarau <ddzmitry@yahoo.com>
2016-11-15 21:50:47 -0500

    Created PHP and Form

---
+++ assets/JS/script.js
@@ -0,0 +1,37 @@
+(function(){
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function(){
+
+
+	var c = document.getElementById('current-time');
+
+	
+
+	setInterval(updateTime, 1000);
+
+	function updateTime() {
+		var d = new Date();
+	var hours = d.getHours(),
+		ampm = 'AM';
+				if (hours > 12) {
+					hours -=12;
+					ampm = 'PM';
+				}
+
+				var sep = ':';
+				if (d.getSeconds() % 2 === 1) sep = ' '
+
+				var min = d.getMinutes();
+			if (min < 10) min = '0' + min + ampm;
+
+
+	c.innerHTML = hours + sep + min + ampm;
+
+
+	}
+
+});
+
+
+})();
